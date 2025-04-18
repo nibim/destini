cf0d16087af3f12eeca23a2359bd3fa216d1eda6
nibim <nibm@kth.se>
2025-04-18 15:11:23 +0200

    first commit

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -13,12 +13,24 @@ class ViewController: UIViewController {
     @IBOutlet weak var storyLabel: UILabel!
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
-    
+
+    let story0 :String = "You see a fork in the road."
+    let choice1 :String = "Take a left."
+    let choice2 :String = "Take a right."
+    let example = Story(title: String?, choice1: <#T##String?#>, choice2: <#T##String?#>)
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        storyLabel.text = story0
+        choice1Button.setTitle(choice1, for: .normal)
+        choice2Button.setTitle(choice2, for: .normal)
+
+
 
     }
-
+    @IBAction func choiceMade(_ sender: UIButton) {
+    }
+    
 
 }
 
