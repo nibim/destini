c2592c169569e1a2d48fa90f17e9065af6097d0d
nibim <nibm@kth.se>
2025-04-20 10:57:49 +0200

    Feature: a struct for forming the dataStructure having 5 props.

--- Destini-iOS13/Model/Story.swift
+++ Destini-iOS13/Model/Story.swift
@@ -9,7 +9,9 @@
 import Foundation
 
 struct Story {
-    let title: String?
-    let choice1: String?
-    let choice2: String?
+    let title: String
+    let choice1: String
+    let choice1Destination: Int
+    let choice2: String
+    let choice2Destination: Int
 }
