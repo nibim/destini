188fc0e38e6ed0e5446f44282d96d80567cb1edd
nibim <nibm@kth.se>
2025-04-20 10:53:42 +0200

    File: VC was written with the help of viewDidLoad and the press button choiceMade where we change the data including storyNumber to keep track where in array we are and where we are going

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -14,21 +14,24 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
 
-    let story0 :String = "You see a fork in the road."
-    let choice1 :String = "Take a left."
-    let choice2 :String = "Take a right."
-    let example = Story(title: String?, choice1: <#T##String?#>, choice2: <#T##String?#>)
+    let stroyTitle :String = ""
+    var choice1 :String = ""
+    var choice2 :String = ""
+    var storyBrain = StoryBrain()
+    var storyNumber : Int = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        storyLabel.text = story0
-        choice1Button.setTitle(choice1, for: .normal)
-        choice2Button.setTitle(choice2, for: .normal)
-
-
-
+        storyLabel.text = storyBrain.getStoryTitle()
+        choice1Button.setTitle(storyBrain.getChoice1(), for: .normal)
+        choice2Button.setTitle(storyBrain.getChoice2(), for: .normal)
     }
+    
     @IBAction func choiceMade(_ sender: UIButton) {
+        storyBrain.nextStory(userChoice: sender.currentTitle!)
+        storyLabel.text = storyBrain.getStoryTitle()
+        choice1Button.setTitle(storyBrain.getChoice1(), for: .normal)
+        choice2Button.setTitle(storyBrain.getChoice2(), for: .normal)
     }
     
 
